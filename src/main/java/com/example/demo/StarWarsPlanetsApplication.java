0445d4e8d4e358388d524b98bd1abae0d4438e3e
Otavio <otavioarc@outlook.com>
2024-08-02 02:31:52 +0000

    Project Initiation

---
+++ src/main/java/com/example/demo/StarWarsPlanetsApplication.java
@@ -0,0 +1,13 @@
+package com.ame.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarWarsPlanetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarWarsPlanetsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo/StarWarsPlanetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ame.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarWarsPlanetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
